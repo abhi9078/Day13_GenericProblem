3b78461fc40765ad442f6c85924329c93c6d6a28
Abhilash <abhilashmeher1234@gmail.com>
2022-03-22 11:01:24 +0530

    Added program File for UC_1

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Day13GenericProblem
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Greater between 3 integer program ");
+            Console.WriteLine("Enter 3 Integer Value");
+            int fst=int.Parse(Console.ReadLine());
+            int snd=int.Parse(Console.ReadLine());
+            int trd=int.Parse(Console.ReadLine());
+            UC_1_MaxInteger.MaxInteger(fst, snd, trd);
+        }
+    }
+}
