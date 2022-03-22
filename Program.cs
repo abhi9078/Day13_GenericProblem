b84730ed5415b59972c1c937e7eead2f998913b6
Abhilash <abhilashmeher1234@gmail.com>
2022-03-22 12:35:22 +0530

    Added program file for UC_4

--- Program.cs
+++ Program.cs
@@ -9,18 +9,16 @@ namespace Day13GenericProblem
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to program ");
-            
-            Refactor2<int> refactorint = new Refactor2<int>(10,20,30);
-            int max1 = refactorint.MaxValueOf(10, 20, 30);
-            Console.WriteLine("Maximum value of int is: "+max1);
+            Console.WriteLine("Enter Length of Array");
+            int len = int.Parse(Console.ReadLine());
+            int[] arr = new int[len];
+            Console.WriteLine("Enter elements of array");
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = int.Parse(Console.ReadLine());
+            }
 
-            Refactor2<double> refactorfloat = new Refactor2<double>(10.10, 20.20, 30.30);
-            double max2 = refactorfloat.MaxValueOf(10.10, 20.20, 30.30);
-            Console.WriteLine("Maximum value of float is: " + max2);
-
-            Refactor2<string> refactorstring = new Refactor2<string>("Abhi", "avi", "lol");
-            string max3 = refactorstring.MaxValueOf("Abhi", "avi", "lol");
-            Console.WriteLine("Maximum value of string is: " + max3);
+            UC_4_MaxMethod.Max(arr);
         }
     }
 }
---
+++ UC_4_MaxMethod.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day13GenericProblem
+{
+    internal class UC_4_MaxMethod
+    {
+        public static void Max(int[] arr)
+        {
+            
+            Array.Sort(arr);
+            int max = arr.Length - 1;
+            Console.WriteLine("The maximum integer is: "+arr[max]);
+        }
+    }
+}
